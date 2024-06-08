3082d622039b82028c5391e964369d1bd8aeaa32
raoakash01 <raoakash269@gmail.com>
2024-06-08 11:06:54 +0530

    video2x

--- src/components/ignore.jsx
+++ src/components/ignore.jsx
@@ -2,17 +2,18 @@ import React, { useRef, useEffect } from 'react';
 import './ignore.css';
 export default function Ignore() {
     const videoRef = useRef(null);
-  
+    
     useEffect(() => {
       const video = videoRef.current;
+      video.playbackRate = 2.5; // increased video speed
       video.play();
     }, []);
-  
+    
     return (
       <>
         <video className="ignore" ref={videoRef} autoPlay loop>
           <source
-            src="https://d2rndhzlwwne8a.cloudfront.net/ignorance.mp4"  // Updated source
+            src="https://d2rndhzlwwne8a.cloudfront.net/ignorance.mp4"
             type="video/mp4"
           />
         </video>
